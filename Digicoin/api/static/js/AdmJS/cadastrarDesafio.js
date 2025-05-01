165cfc73db9e9bf45b882d4b2ae0f18d18e678a6
ArthurGuedes <arthurguedes0206@gmail.com>
2025-04-30 21:38:24 -0400

    30/04

--- Digicoin/api/static/js/AdmJS/cadastrarDesafio.js
+++ Digicoin/api/static/js/AdmJS/cadastrarDesafio.js
@@ -1,70 +1,47 @@
-async function Cadastrar(evento) {
-    evento.preventDefault();
+async function CadastrarDesafio(event) {
+    event.preventDefault();
+    
+    const nome = document.getElementById('nomeDesafio-form').value;       
+    const valor = document.getElementById('valorDesafio-form').value;     
+    const descricao = document.getElementById('descricao-form').value;    
+    const dataInicio = document.getElementById('inicioDesafio-form').value;
+    const dataFim = document.getElementById('fimDesafio-form').value;      
+    const idCampanha = document.getElementById('campanha-form').value;    
+    const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-    const formulario = evento.target;
-    const id = formulario.querySelector("#id").value;
-    const nomeDesafio = formulario.querySelector("#nomeDesafio").value;
-    const valorDesafio = formulario.querySelector("#valorDesafio").value;
-    const descricao = formulario.querySelector("#descricao").value;
-    const inicioDesafio = formulario.querySelector('#inicioDesafio').value;
-    const fimDesafio = formulario.querySelector('#fimDesafio').value;
-    const campanha = formulario.querySelector("#campanha").value;
-
-    const csrf = formulario.querySelector('[name=csrfmiddlewaretoken]').value;
-
-    try {
-        if (inicioDesafio <= fimDesafio) {
-            const dados = {
-                nome: nomeDesafio,
-                valor: valorDesafio,
-                dataInicio: inicioDesafio,
-                descricao: descricao,
-                dataFim: fimDesafio,
-                idCampanha: campanha
-            };
-
-            const headers = { 'X-CSRFToken': csrf };
-
-            if (id) {
-                const response = await apiRequest(`/api/desafio/${id}`, "PUT", dados, headers);
-                console.log(response || "Erro ao editar", response);
-            } else {
-                const response = await apiRequest("/api/desafio/", "POST", dados, headers);
-                console.log(response || "Erro ao cadastrar", response);
-            }
-        } else {
-            alert("A Data de início não pode ser maior que a Data de fim do Desafio");
-        }
-    } catch (error) {
-        console.error("Erro:", error);
+    if(!nome || !valor || !dataInicio){
+        alert("Preencha todos os campos corretamente.");
+        return;
+        
     }
+    else if (dataFim < dataInicio) {
+        
+        alert("A data de fim deve ser maior que a data de inicio.");
+        return;
+    }
+    else{
+        const response = await apiRequest('/api/desafio/','POST',
+        {
+            nome: nome,
+            valor: valor,
+            dataInicio: dataInicio,
+            descricao: descricao,
+            dataFim: dataFim,
+            idCampanha: idCampanha
+            },{ 'X-CSRFToken': csrf }
+        );
+        console.log(response);    
+        document.getElementById('nomeDesafio-form').value = '';
+        document.getElementById('valorDesafio-form').value = '';
+        document.getElementById('descricao-form').value = '';
+        document.getElementById('inicioDesafio-form').value = '';
+        document.getElementById('fimDesafio-form').value = '';
+        document.getElementById('campanha-form').value = '';
+        alert('Desafio cadastrado com sucesso!');
+        window.location.reload();
+    }
+    
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    document.querySelectorAll(".formDesafio").forEach(form => {
-        form.addEventListener("submit", Cadastrar);
-    });
-
-    const popUpAdicionarDesafio = document.getElementById('popUpAdicionarDesafio');
-    const addDesafio = document.getElementById('addDesafio');
-
-    addDesafio.addEventListener('click', () => {
-        popUpAdicionarDesafio.showModal();
-    });
-
-    document.querySelectorAll(".iconeX").forEach(icone => {
-        icone.addEventListener('click', () => {
-            const dialog = icone.closest("dialog");
-            if (dialog) dialog.close();
-        });
-    });
-
-    document.querySelectorAll(".botaoEditar").forEach(botao => {
-        botao.addEventListener("click", () => {
-            const id = botao.getAttribute("data-id");
-            const dialog = document.getElementById(`popUpEditarDesafio-${id}`);
-            if (dialog) dialog.showModal();
-        });
-    });
-});
+const criarCampanhaForm = document.getElementById('formDesafio');
+criarCampanhaForm.addEventListener('submit', CadastrarDesafio);
--- Digicoin/api/static/js/AdmJS/listaDeDesafios.js
+++ Digicoin/api/static/js/AdmJS/listaDeDesafios.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Fechar o dialog quando clicar no ícone X
+
     const iconesFechar = document.querySelectorAll(".iconeX");
     iconesFechar.forEach(icone => {
         icone.addEventListener("click", () => {
@@ -40,3 +40,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 })
+// formCadastrarDesafio
+async function EditarDesafio(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const id = form.querySelector('#id').value;
+    const nomeDesafio = form.querySelector('#nomeDesafio').value;
+    const valorDesafio = form.querySelector('#valorDesafio').value;
+    const descricao = form.querySelector('#descricao').value;
+    const campanha = form.querySelector('#campanha').value;
+    const dataInicio = form.querySelector('#inicioDesafio').value;
+    const dataFim = form.querySelector('#fimDesafio').value;
+    const csrf = form.querySelector('[name=csrfmiddlewaretoken]').value;
+
+    
+    if (!nomeDesafio || !valorDesafio) {
+        alert("Nome do desafio e valor do desafio devem ser preenchidos.");
+        return;
+    }
+    if (dataFim < dataInicio) {
+        
+        alert("A data de fim deve ser maior que a data de inicio.");
+        return;
+    }
+
+    const response = await apiRequest(`/api/desafio/${id}/`, 'PUT', {nome:nomeDesafio, valor:valorDesafio, descricao:descricao, campanha:campanha}, {'X-CSRFToken':csrf});
+    console.log(response);
+    
+    window.location.reload();
+}
+
+const forms = document.querySelectorAll('form[id^="formCadastrarDesafio"]');
+forms.forEach(form => {
+    form.addEventListener('submit', EditarDesafio);
+});
