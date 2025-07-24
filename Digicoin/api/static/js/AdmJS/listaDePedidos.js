bc73f784962647e272e4438b67d2d2bbb6398f23
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2025-07-23 21:25:28 -0400

    atualizado

--- Digicoin/api/static/js/AdmJS/listaDePedidos.js
+++ Digicoin/api/static/js/AdmJS/listaDePedidos.js
@@ -6,3 +6,43 @@ function toggleItens(id) {
         el.style.display = 'none';
     }
 }
+
+async function atulizarPedido(idPedido) {
+    try {
+        let response = await fetch(`/api/compra/${idPedido}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            },
+            body: JSON.stringify({ pedido: "Concluído" })
+        });
+
+        if (response.ok) {
+            console.log('Pedido concluído com sucesso!');
+        } else {
+            console.error('Erro ao concluir o pedido:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const botoes = document.querySelectorAll('#botaoConcluir');
+
+    botoes.forEach(botao => {
+        botao.addEventListener('click', function (event) {
+            // Sobe até a div com a classe 'itensCompra-listaDePedidos'
+            const divPai = event.target.closest('.itensCompra-listaDePedidos');
+            if (divPai) {
+                const idCompra = divPai.id.replace('itens-', '');
+                console.log('ID da compra:', idCompra);
+
+                atulizarPedido(idCompra);
+                window.location.reload();
+                    
+            }
+        });
+    });
+});
