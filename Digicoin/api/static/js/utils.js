cc67fde80a2de2694462c0a7dd5817b1768bbbb4
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2025-08-18 19:56:16 -0400

    Update utils.js

--- Digicoin/api/static/js/utils.js
+++ Digicoin/api/static/js/utils.js
@@ -1,15 +1,15 @@
 async function apiRequest(url, method = 'GET', body = null, headers = {}) {
     try {
-        const isFormData = body instanceof FormData;
-
         const config = {
             method,
-            headers: isFormData ? headers : {
+            headers: {
                 'Content-Type': 'application/json',
                 ...headers
             },
-            body: isFormData ? body : JSON.stringify(body)
         };
+        if (body) {
+            config.body = JSON.stringify(body);
+        }
         const response = await fetch(url, config);
         if (!response.ok) {
             throw new Error(`Erro: ${response.status} - ${response.statusText}`);
